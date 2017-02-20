b6e19060aa7db6399f8fc2eb0df4157143d35883
Mike Liao <mikeliao97@gmail.com>
2017-02-19 19:33:56 -0800

    implement ForageBee + retiredForageBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,6 +1,18 @@
 class HoneyMakerBee extends Bee {
   // TODO..
   constructor() {
+  	super();
   	
+  	this.age = 10;	
+  	this.job = "make honey";
+  	this.honeyPot = 0;	
+  }
+
+  makeHoney() {
+  	this.honeyPot += 1;
+  }
+
+  giveHoney() {
+  	this.honeyPot -=1;
   }
 };
